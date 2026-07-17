e9a2e94b727819d4f3d09b002482dd338402e006
Arutselvan-Btech <arutselvanmani2008@gmail.com>
2026-07-17 22:50:19 +0530

    Update API URL to new Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
- baseURL: "https://taskflow-sfj5.onrender.com",
+  baseURL: "https://taskflow-sfj5.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
